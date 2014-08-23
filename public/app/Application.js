c1452a81ed306484efc4730f4fd916e165c9f737
Edivan de Castro <edivandecastro@gmail.com>
2014-08-23 15:05:43 -0300

    Implementando a tela de carregamento da aplicação.

--- public/app/Application.js
+++ public/app/Application.js
@@ -13,5 +13,35 @@ Ext.define('AgeStoneVideos.Application', {
 
     stores: [
         // TODO: add stores here
-    ]
+    ],
+
+    splashscreen: {},
+
+    init: function () {
+        splashscreen = Ext.getBody().mask('Loading application', 'splashscreen');
+        splashscreen.addCls('splashscreen');
+
+        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+            cls: 'x-splash-icon'
+        });
+    },
+
+    launch: function () {
+    
+        var task = new Ext.util.DelayedTask(function () {
+
+            splashscreen.fadeOut({
+                duration: 1000,
+                remove:true
+            });
+
+            splashscreen.next().fadeOut({
+                duration: 1000,
+                remove:true
+            });
+
+            //Ext.getBody().unmask();
+        });
+        task.delay(2000);
+    },
 });
